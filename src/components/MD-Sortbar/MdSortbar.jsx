889934026c55e7dbb1eeab5ba95f95035ea6e667
Sina Beyki <mrsaher83@gmail.com>
2023-08-10 11:03:04 +0330

    Medium Device SortBar Created.

---
+++ src/components/MD-Sortbar/MdSortbar.jsx
@@ -0,0 +1,17 @@
+const MdSortbar = () => {
+  return (
+    <div className='bg-white px-4 flex items-center gap-x-4 text-gray-400 rounded-lg'>
+      <div className='bg-orange-100 rounded-md p-2'>
+        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-7 h-7 stroke-orange-500'>
+          <path strokeLinecap='round' strokeLinejoin='round' d='M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75' />
+        </svg>
+      </div>
+      <button className='py-4'>محبوب ترین</button>
+      <button className='py-4'>پرفروش ترین</button>
+      <button className='py-4'>ارزان ترین</button>
+      <button className='py-4'>گران ترین</button>
+    </div>
+  );
+};
+
+export default MdSortbar;
