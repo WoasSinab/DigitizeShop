cc7fe2b4899af6f410d531d9d4aa53a387e937a0
Sina Beyki <mrsaher83@gmail.com>
2023-08-14 19:29:01 +0330

    some change.

--- src/components/ProductGridSec/ProductGridSec.jsx
+++ src/components/ProductGridSec/ProductGridSec.jsx
@@ -1,3 +1,4 @@
+import MdNavbar from "../MD-Navbar/MdNavbar";
 import MdSidebar from "../MD-Sidebar/MdSidebar";
 import MdSortbar from "../MD-Sortbar/MdSortbar";
 import ProductSection from "../ProductSection/ProductSection";
@@ -5,18 +6,26 @@ import ProductSection from "../ProductSection/ProductSection";
 const ProductGridSec = () => {
   return (
     <div>
+      {/* MD Device Content  */}
+
+        {/* MD Navbar */}
+        <div className="col-span-8 lg:col-span-9 sticky hidden md:block ">
+          <MdNavbar />
+        </div>
+
       <div className='grid grid-cols-12 md:grid-rows-[55px_minmax(500px,_1fr)] md:gap-8'>
-        {/* sideBar */}
+        {/* MD sideBar */}
         <div className='col-span-4 lg:col-span-3 hidden md:block row-span-2 '>
           <MdSidebar />
         </div>
 
-        {/* sortBar */}
+
+        {/* MD sortBar */}
         <div className='col-span-8 lg:col-span-9  hidden md:block '>
           <MdSortbar />
         </div>
 
-        {/* productSec */}
+        {/* Page productSec */}
         <div className='col-span-12 md:col-span-8 lg:col-span-9 '>
           <ProductSection />
         </div>
