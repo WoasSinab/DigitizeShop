b6f6f418f035601d947b6375b321e71708b3f02e
Sina Beyki <mrsaher83@gmail.com>
2023-08-15 18:50:17 +0330

    Sm bottom Navbar Created.

---
+++ src/components/BotttomNav/ButttomNav.jsx
@@ -0,0 +1,47 @@
+const BotttomNav = () => {
+  return (
+    <div className='fixed bottom-0 left-0 right-0 w-full rounded-tr-md rounded-tl-md bg-white px-4 md:hidden text-gray-500'>
+      {/* Navigation */}
+      <nav className='w-full'>
+        <ul className='flex items-center justify-center gap-x-2 w-full'>
+          {/* Home */}
+          <li className='text-slate-900 w-full'>
+            <a href='#' className='flex items-center justify-center py-4 gap-x-2'>
+              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-6 h-6'>
+                <path d='M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z' />
+                <path d='M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z' />
+              </svg>
+              <span>خانه</span> {/* Home */}
+            </a>
+          </li>
+          {/* Second Icon */}
+          <li className='w-full'>
+            <a href='#' className='flex items-center justify-center py-4'>
+              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-6 h-6'>
+                <path fill-rule='evenodd' d='M3 6a3 3 0 013-3h2.25a3 3 0 013 3v2.25a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm9.75 0a3 3 0 013-3H18a3 3 0 013 3v2.25a3 3 0 01-3 3h-2.25a3 3 0 01-3-3V6zM3 15.75a3 3 0 013-3h2.25a3 3 0 013 3V18a3 3 0 01-3 3H6a3 3 0 01-3-3v-2.25zm9.75 0a3 3 0 013-3H18a3 3 0 013 3V18a3 3 0 01-3 3h-2.25a3 3 0 01-3-3v-2.25z' clip-rule='evenodd' />
+              </svg>
+            </a>
+          </li>
+          {/* Third Icon */}
+          <li className='w-full'>
+            <a href='#' className='flex items-center justify-center py-4'>
+              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-6 h-6'>
+                <path fill-rule='evenodd' d='M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z' clip-rule='evenodd' />
+              </svg>
+            </a>
+          </li>
+          {/* Fourth Icon */}
+          <li className='w-full'>
+            <a href='#' className='flex items-center justify-center py-4'>
+              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-6 h-6'>
+                <path d='M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z' />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default BotttomNav;
